526c13221ca59db02d3dc337d893406793d92874
Justin Love <git@JustinLove.name>
2016-05-03 14:39:47 -0500

    remove lobby refresh and add uninstall nag dialog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Icon Extensions
 
+## 2.1.0
+
+Remove lobby refresh and add uninstall nag dialog
+
 ## 2.0.0 for > 89755
 
 This mod is obsolete and should be uninstalled if possible.
---
+++ ui/mods/icon_extensions/warning.js
@@ -0,0 +1,34 @@
+(function() {
+  var setupDialog = function() {
+    $(".icon_extensions_dialog").dialog({
+      dialogClass: "no-close",
+      draggable: false,
+      resizable: false,
+      height: 400,
+      width: 600,
+      modal: true,
+      autoOpen: true,
+      buttons: {
+          "EXIT": function () {
+              model.exit();
+          },
+          "LATER": function () {
+              $(this).dialog("close");
+          }
+      }
+    });
+  }
+
+  //load html dynamically
+  var loadTemplate = function (element, url, model) {
+    element.load(url, function () {
+      console.log("Loading html " + url);
+      ko.applyBindings(model, element.get(0));
+      setupDialog()
+    });
+  };
+
+  var container = $('<div id="insertion_point"></div>')
+  container.appendTo('body')
+  loadTemplate(container, 'coui://ui/mods/icon_extensions/warning.html', model);
+})()
